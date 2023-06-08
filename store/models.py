98b06c75695a80f535ef8067ab6b0c2f1ce753b2
sengxingstx.dev <sengxingstx.dev@gmail.com>
2023-06-08 11:17:51 +0700

    first commit

---
+++ store/models.py
@@ -0,0 +1,89 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    digital = models.BooleanField(default=False)
+    img = models.ImageField(upload_to="products/", null=True)
+
+    def __str__(self):
+        return self.name
+    
+    @property
+    def image_url(self):
+        try:
+            url = self.img.url
+        except:
+            url = ''
+        return url
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.id}'
+
+    @property
+    def shipping(self):
+        shipping = False
+        order_items = self.orderitem_set.all()
+        for i in order_items:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
+    
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
---
+++ store/views.py
@@ -0,0 +1,112 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import json
+import datetime
+from .utils import cookie_cart, cart_data, guest_order
+from .models import (
+    Customer,
+    Product,
+    Order,
+    OrderItem,
+    ShippingAddress
+)
+
+
+def store(request):
+    data = cart_data(request)
+    cart_items = data['cart_items']
+
+    products = Product.objects.all()
+    context = {
+        'products': products,
+        'cart_items': cart_items
+    }
+    return render(request, 'store/store.html', context)
+
+
+def cart(request):
+    data = cart_data(request)
+    items = data['items']
+    order = data['order']
+    cart_items = data['cart_items']
+
+    context = {
+        'items': items,
+        'order': order,
+        'cart_items': cart_items
+    }
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    data = cart_data(request)
+    items = data['items']
+    order = data['order']
+    cart_items = data['cart_items']
+
+    context = {
+        'items': items,
+        'order': order,
+        'cart_items': cart_items
+    }
+    return render(request, 'store/checkout.html', context)
+
+
+def update_item(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    order_item, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        order_item.quantity = (order_item.quantity + 1)
+    elif action == 'remove':
+        order_item.quantity = (order_item.quantity - 1)
+    
+    order_item.save()
+
+    if order_item.quantity <= 0:
+        order_item.delete()
+
+    return JsonResponse('Item was added', safe=False)
+
+
+def process_order(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer,
+            complete=False
+        )
+
+    else:
+        customer, order = guest_order(request, data)
+
+    # user or guest user checkout have to save to db
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+
+    if total == float(order.get_cart_total):
+        order.complete = True
+    order.save()
+
+    # print('Total1 == Total2:', total == float(order.get_cart_total))
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+        )
+
+    return JsonResponse('Payment complete!', safe=False)
