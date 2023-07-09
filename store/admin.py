0a007f9e1314fcea41842892260da46ad9c9731f
sengxingstx.dev <sengxingstx.dev@gmail.com>
2023-07-09 22:45:57 +0700

    updated

--- store/admin.py
+++ store/admin.py
@@ -9,11 +9,69 @@ from .models import (
 )
 
 
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(ShippingAddress)
-admin.site.register(ConfirmPayment)
-admin.site.register(Income)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'sirname','sex', 'village', 'district', 'province'
+    )
+    search_fields = ('name',)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'price', 'detail', 'img', 'amount'
+    )
+    search_fields = ('name',)
+
+
+class ShippingAddressAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'customer', 'order', 'address', 'city', 'state',
+        'zipcode', 'tel', 'delivery', 'date_added'
+    )
+    search_fields = ('name',)
+
+
+class ShippingAddressAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'customer', 'order', 'address', 'city', 'state',
+        'zipcode', 'tel', 'delivery', 'date_added'
+    )
+    search_fields = ('name',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'customer', 'complete', 'date_ordered'
+    )
+    search_fields = ('customer',)
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'product', 'order', 'quantity', 'date_added'
+    )
+    search_fields = ('product',)
+
+
+class ConfirmPaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'date_payment', 'total', 'comment', 'payment_photo'
+    )
+    search_fields = ('comment',)
+
+
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'price', 'user_id', 'date_received',
+    )
+    search_fields = ('price',)
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
+admin.site.register(ShippingAddress, ShippingAddressAdmin)
+admin.site.register(ConfirmPayment, ConfirmPaymentAdmin)
+admin.site.register(Income, IncomeAdmin)
 admin.site.register(Outcome)
